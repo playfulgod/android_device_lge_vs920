8e1163d5e3e8790b96689095404674aef72f4d0b
death2all110 <deatheatergrim@gmail.com>
2012-06-05 18:47:03 -0500

    Initial Spectrum commit for CM9 support

---
+++ cm.mk
@@ -0,0 +1,24 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
+TARGET_BOOTANIMATION_NAME := vertical-720x1280
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/spectrum/spectrum.mk)
+
+PRODUCT_NAME := cm_spectrum
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := Spectrum 4G
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := spectrum
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=i_vzw BUILD_FINGERPRINT=Verizon/lge_iproj/VS920:4.0.4/IMM76D/VS920ZV7.47d6aee9:user/release-keys PRIVATE_BUILD_DESC="i_vzw-user 4.0.4 IMM76D eng.yeoncheol.hong release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
---
+++ spectrum.mk
@@ -0,0 +1,115 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/lge/spectrum/spectrum-vendor.mk)
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/lge/spectrum/overlay
+
+# This device is xhdpi.  However the platform doesn't
+# currently contain all of the bitmaps at xhdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the xhdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.lge.rc:root/init.lge.rc \
+    $(LOCAL_PATH)/ueventd.lge.rc:root/ueventd.lge.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.lge.usb.rc:root/init.lge.usb.rc \
+    $(LOCAL_PATH)/prebuilt/boot_logo_00000.rle:root/bootimages/boot_logo_00000.rle \
+    $(LOCAL_PATH)/prebuilt/init.qcom.sh:root/init.qcom.sh \
+    $(LOCAL_PATH)/prebuilt/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    $(LOCAL_PATH)/prebuilt/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
+    $(LOCAL_PATH)/prebuilt/init.qcom.mdm_links.sh:system/etc/init.qcom.mdm_links.sh
+
+## Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/atcmd_virtual_kbd.kl:system/usr/keylayout/atcmd_virtual_kbd.kl \
+    $(LOCAL_PATH)/config/ffa-keypad_qwerty.kl:system/usr/keylayout/ffa-keypad_qwerty.kl \
+    $(LOCAL_PATH)/config/i_atnt-keypad.kl:system/usr/keylayout/i_atnt-keypad.kl \
+    $(LOCAL_PATH)/config/pmic8058_pwrkey.kl:system/usr/keylayout/pmic8058_pwrkey.kl \
+    $(LOCAL_PATH)/config/synaptics_ts.kl:system/usr/keylayout/synaptics_ts.kl \
+    $(LOCAL_PATH)/config/synaptics_ts.idc:system/usr/idc/synaptics_ts.idc \
+    $(LOCAL_PATH)/config/thermald.conf:system/etc/thermald.conf
+
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/nvram.txt:system/etc/wl/nvram.txt \
+    $(LOCAL_PATH)/config/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/prebuilt/wireless.ko:system/lib/modules/wireless.ko
+
+
+# Permission files
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
+# HW HALS
+
+PRODUCT_PACKAGES += \
+    hdmid \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    gralloc.msm8660 \
+    hwcomposer.msm8660 \
+    copybit.msm8660 \
+    lights.msm8660 \
+    gps.spectrum \
+    audio.primary.spectrum \
+    audio_policy.msm8660 \
+    audio.a2dp.default
+
+# OMX
+#
+PRODUCT_PACKAGES += \
+    libstagefrighthw \
+    libdivxdrmdecrypt \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libmm-omxcore \
+    libOmxCore
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/init.qcom.bt.sh:system/bin/init.qcom.bt.sh
+
+PRODUCT_PACKAGES += \
+    hcitool \
+    hciconfig \
+    hwaddrs
+
+# Charger mode
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_spectrum
+PRODUCT_DEVICE := spectrum
+PRODUCT_MODEL := LG-spectrum
+PRODUCT_MANUFACTURER := LGE
